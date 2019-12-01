4dd5ee4d027f6b40f0b9345824e82b812c103574
Adam ? <adamznow@gmail.com>
2019-11-30 21:45:51 -0800

    Initial Commit, seems to build valid .dlls and .sos

---
+++ Il2CppHook/src/Il2CppHook.cpp
@@ -0,0 +1,12 @@
+﻿// Il2CppHook.cpp : Defines the entry point for the application.
+//
+
+#include "../include/Il2CppHook.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
